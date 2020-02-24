8cd84d75a23a5e371fe84c3a4f42429c05f87f49
Jack Lansing <john.a.lansing@gmail.com>
2020-02-24 15:56:56 -0500

    added initial event handlers

--- scripts/Bookmarks.js
+++ scripts/Bookmarks.js
@@ -4,14 +4,23 @@ import QueryUtils from './QueryUtils.js';
 
 const clickNewBookmark = () => {
   //Click handler for new bm
+  $('#root').on('click', '.js-new-button', event => {
+    console.log('clicked');
+  });
 };
 
 const clickFilterBookmark = () => {
   //Click handler for bm filter
+  $('#root').on('change', '#filter-rating', event => {
+    console.log($('#filter-rating').val());
+  });
 };
 
 const clickExpandBookmark = () => {
   //Click handler for expand bm details
+  $('#root').on('click', '.js-bm-expand', event => {
+    console.log('Expand details');
+  });
 };
 
 const clickCloseBookmark = () => {
@@ -31,8 +40,50 @@ const clickCreateBookmark = () => {
   //Must check all required details are provided
 };
 
-export default {
-  clickNewBookmark,
-  clickFilterBookmark,
-  clickExpandBookmark
+const generateStartHtml = () => {
+  return `
+  <h1>My Bookmarks</h1>
+      <button class="new-button js-new-button">New</button>
+      <form class="filter-menu js-filter-menu">
+        <label for="filter-rating">Minimum rating</label>
+        <select id="filter-rating">
+          <option>None</option>
+          <option>1 star</option>
+          <option>2 star</option>
+          <option>3 star</option>
+          <option>4 star</option>
+          <option>5 star</option>
+        </select>
+      </form>
+      <ul class="bookmark-list js-bookmark-list">
+        <li class="bookmark-item js-bookmark-item">
+          <button
+            class="bm-expand js-bm-expand"
+            role="button"
+            aria-expanded="false"
+          >
+            <span class="bm-title js-bm-title">Title</span
+            ><span class="bm-rating js-bm-rating">*****</span>
+          </button>
+        </li>
+      </ul>
+  `;
 };
+
+const render = (target, component) => {
+  //Simple flexible render
+  $(target).html(component);
+};
+
+const setupEventListners = () => {
+  clickNewBookmark();
+  clickFilterBookmark();
+  clickExpandBookmark();
+};
+
+const main = () => {
+  render('#root', generateStartHtml());
+  setupEventListners();
+};
+
+$(main);
